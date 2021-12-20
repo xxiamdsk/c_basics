23b4c19bf4f742727181045ee480b904d8c6691b
Deepak Singh Kushwaha <95338176+xxiamdsk@users.noreply.github.com>
2021-12-20 10:22:36 +0530

    Uploaded 13_simple_interest.c

---
+++ 02_operators/13_simple_interest.c
@@ -0,0 +1,25 @@
+// // C program calculate Simple Interest
+
+// // Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// // Main Function Start
+int main()
+{
+    double principalAmount, rateOfInterest, time, totalAmount, simpleInterest;
+    printf("Enter Pricipal Amount => ");
+    scanf("%lf", &principalAmount);
+    printf("Enter Rate of Interest => ");
+    scanf("%lf", &rateOfInterest);
+    printf("Enter Time In Years => ");
+    scanf("%lf", &time);
+    simpleInterest = principalAmount * rateOfInterest * time / 100;
+    totalAmount = principalAmount + simpleInterest;
+    printf("\nSimple Interest => %.2lf", simpleInterest);
+    printf("\nTotal Amount  => %.2lf", totalAmount);
+
+    getch();
+    return 0;
+}
+// // Main Function End
