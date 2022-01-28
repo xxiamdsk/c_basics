5943fdf35c639f956d66ba54c29a7d986dc0d06b
Deepak Singh Kushwaha <95338176+xxiamdsk@users.noreply.github.com>
2022-01-28 18:54:52 +0530

    Uploaded 37_all_prime_between_two_nums.c

---
+++ 04_iterative_control_instructions/37_all_prime_between_two_nums.c
@@ -0,0 +1,52 @@
+// // C program to print all Prime numbers between two given numbers
+
+// // Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <stdlib.h>
+
+// // Main Function Start
+int main()
+{
+
+    int beg, end, i, primeNumFound = 0;
+    printf("\nEnter Two Numbers to Print All Prime Numbers Between Them => ");
+    scanf("%d%d", &beg, &end);
+
+    if (beg > end)
+        beg = (beg + end) - (end = beg);
+
+    printf("\n>>>>>>>>> Prime Numbers Between %d and %d <<<<<<<<<<<<<<<\n", beg, end);
+
+    while (beg <= end)
+    {
+        if (beg < 2)
+        {
+            beg++;
+            continue;
+        }
+
+        for (i = 2; i <= sqrt(beg); i++)
+        {
+            if (beg % i == 0)
+                break;
+        }
+
+        if (i > sqrt(beg))
+        {
+            printf("%d ", beg);
+            primeNumFound = 1;
+        }
+
+        beg++;
+    }
+
+    if (!primeNumFound)
+        printf("\nThere Are No Prime Numbers Between Entered Numbers...");
+
+    printf("\n");
+    getch();
+    return 0;
+}
+// // Main Function End
