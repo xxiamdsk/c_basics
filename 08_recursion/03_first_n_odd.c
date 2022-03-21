1bf198d6dd24d4e3033944a74c2ef368a410f831
Deepak Singh Kushwaha <95338176+xxiamdsk@users.noreply.github.com>
2022-03-21 17:35:14 +0530

    Uploaded 03_first_n_odd.c

---
+++ 08_recursion/03_first_n_odd.c
@@ -0,0 +1,43 @@
+// // Write a recursive function to print first N odd natural numbers.
+
+// // Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+// // Function Declaration (Prototype)
+void firstNOddNaturalNums(int);
+
+// // Main Function Start
+int main()
+{
+    int n;
+    printf("\nEnter N to Print First N Odd Natural Numbers => ");
+    scanf("%d", &n);
+
+    if (n < 0)
+    {
+        puts("\n!!! Invalid Input, Plz Enter Positive Number...");
+        exit(0);
+    }
+
+    printf("\n>>>>>>> First %d Odd Natural Numbers <<<<<<<\n", n);
+    firstNOddNaturalNums(n);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// // Main Function End
+
+// // Function Definition 👇👇
+
+// // Recursive Function to Print First N Odd Natural Numbers
+void firstNOddNaturalNums(int n)
+{
+    if (n > 0)
+    {
+        firstNOddNaturalNums(n - 1);
+        printf("%d ", n * 2 - 1);
+    }
+}
