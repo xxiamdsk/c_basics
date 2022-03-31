6b1f3a3f01a3b17cd60dd4e4e0c6e4f6dc2aed75
Deepak Singh Kushwaha <95338176+xxiamdsk@users.noreply.github.com>
2022-03-31 14:22:55 +0530

    Uploaded 04_largest.c

---
+++ 09_arrays/04_largest.c
@@ -0,0 +1,35 @@
+// // Write a program to find the largest number stored in an array of size 10. Take array values from the user.
+
+// // Header Files
+#include <stdio.h>
+#include <conio.h>
+
+#define ARRAY_SIZE 10
+
+// // Main Function Start
+int main()
+{
+    int nums[ARRAY_SIZE];
+
+    printf("\nEnter 10 Numbers => ");
+
+    // // Input Numbers
+    for (int i = 0; i < ARRAY_SIZE; i++)
+        scanf("%d", &nums[i]);
+
+    int largest = nums[0];
+
+    // // Find Largest
+    for (int i = 1; i < ARRAY_SIZE; i++)
+    {
+        if (nums[i] > largest)
+            largest = nums[i];
+    }
+
+    printf("\nLargest Number => %d", largest);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// // Main Function End
