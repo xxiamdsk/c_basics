9f59af313db390e3599f7263ec58a9487d7a7d61
Deepak Singh Kushwaha <95338176+xxiamdsk@users.noreply.github.com>
2022-04-28 19:34:43 +0530

    Uploaded 09_sort_string.c

---
+++ 13_strings/09_sort_string.c
@@ -0,0 +1,60 @@
+// // Write a C program to sort a string array in ascending order.
+
+// // Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+#define ARRAY_SIZE 31
+
+// // Function Declaration
+void swap(char *, char *);
+
+// // Main Function Start
+int main()
+{
+    char str[ARRAY_SIZE], length = 0;
+
+    printf("\nEnter Any String (MAX CHARACTERS %d) => ", ARRAY_SIZE - 1);
+    fgets(str, ARRAY_SIZE, stdin);  // // Input String
+    str[strcspn(str, "\n")] = '\0'; // // Replace '\n' character with '\0' in str
+
+    printf("\nString Before Sorting => %s", str);
+
+    // // Find Length
+    while (str[length])
+        length++;
+
+    // // 1st Approach (Bubble Sort)
+    for (int i = 0; i < length - 1; i++)
+    {
+        for (int j = 0; j < length - 1 - i; j++)
+        {
+            if (str[j] > str[j + 1])
+                swap(&str[j], &str[j + 1]);
+        }
+    }
+
+    // // 2nd Approach (Selection Sort)
+    // // for (int i = 0; i < length - 1; i++)
+    // // {
+    // //    for (int j = i + 1; j < length; j++)
+    // //    {
+    // //        if (str[i] > str[j])
+    // //            swap(&str[i], &str[j]);
+    // //    }
+    // // }
+
+    printf("\nString After Sorting => %s", str);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// // Main Function End
+
+// // Function to Swap values of Two Character variables
+void swap(char *ch1, char *ch2)
+{
+    *ch1 = (*ch1 + *ch2) - (*ch2 = *ch1);
+}
