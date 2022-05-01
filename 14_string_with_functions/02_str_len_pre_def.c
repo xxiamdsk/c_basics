856fec6e96f0bea7f8945a8a150c29e9d1934abc
Deepak Singh Kushwaha <95338176+xxiamdsk@users.noreply.github.com>
2022-05-01 06:59:33 +0530

    Uploaded 02_str_len_pre_def.c

---
+++ 14_string_with_functions/02_str_len_pre_def.c
@@ -0,0 +1,28 @@
+// // Write a c program to calculate length of the string using pre-defined function.
+
+// // Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+#define ARRAY_SIZE 31
+
+// // Main Function Start
+int main()
+{
+    char name[ARRAY_SIZE];
+    int length;
+
+    printf("\nEnter Your Name (MAX CHARACTERS %d) => ", ARRAY_SIZE - 1);
+    fgets(name, ARRAY_SIZE, stdin);   // // Input String
+    name[strcspn(name, "\n")] = '\0'; // // Replace '\n' character with '\0' in name
+
+    length = strlen(name); // // Find Length using pre-defined function
+
+    printf("\nYour Name is \"%s\" and Number of Characters in Your Name => %d", name, length);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// // Main Function End
