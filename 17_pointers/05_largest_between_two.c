0aa3917b33790cb00e0bd6c31f4fe1765fcc94f9
Deepak Singh Kushwaha <95338176+xxiamdsk@users.noreply.github.com>
2022-05-18 18:58:51 +0530

    Uploaded 05_largest_between_two.c

---
+++ 17_pointers/05_largest_between_two.c
@@ -0,0 +1,29 @@
+// // Write a program to find the largest number between two numbers using pointers.
+
+// // Header Files
+#include <stdio.h>
+#include <conio.h>
+
+// // Main Function Start
+int main()
+{
+    int num1, num2, largest, *p, *q, *r;
+
+    // // Assign addresses to pointers
+    p = &num1;
+    q = &num2;
+    r = &largest;
+
+    printf("\nEnter Two Numbers to Find Largest => ");
+    scanf("%d%d", p, q);
+
+    // // Find Largest
+    *r = *p > *q ? *p : *q;
+
+    printf("\nLargest => %d", *r);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// // Main Function End
