e9e4fedb9152ac102262944ce358d3758d853d1e
Deepak Singh Kushwaha <95338176+xxiamdsk@users.noreply.github.com>
2022-05-18 05:33:22 +0530

    Uploaded 06_length.c

---
+++ 17_pointers/06_length.c
@@ -0,0 +1,33 @@
+// // Write a program to calculate the length of the string using a pointer.
+
+// // Header Files
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+#define ARRAY_SIZE 31
+
+// // Main Function Start
+int main()
+{
+    char str[ARRAY_SIZE], *ptr;
+    int length = 0;
+
+    // // Assign Address to Pointer
+    ptr = str;
+
+    printf("\nEnter Any String (MAX CHARACTERS %d) => ", ARRAY_SIZE - 1);
+    fgets(ptr, ARRAY_SIZE, stdin);  // // Input String
+    ptr[strcspn(ptr, "\n")] = '\0'; // // Replace '\n' character with '\0'
+
+    // // Find Length
+    while (*(ptr + length))
+        length++;
+
+    printf("\nLength of String => %d", length);
+
+    putch('\n');
+    getch();
+    return 0;
+}
+// // Main Function End
